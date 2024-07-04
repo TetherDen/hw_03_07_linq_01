06e9cb37b8737b5e40183c7c8f584c58f2daaafb
DenisDell <bigdramashow730@gmail.com>
2024-07-04 20:08:16 +0300

    init

---
+++ Device/Device.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_03_07_linq_01
+{
+    internal class Device
+    {
+        public string DeviceName {  get; set; }
+        public string Manufacturer { get; set; }
+        public double Price { get; set; }
+        public Device(string deviceName, string manufacturer, double price)
+        {
+            DeviceName = deviceName;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {DeviceName}, Price: {Price}, Manufacturer: {Manufacturer}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace hw_03_07_linq_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //            Завдання 3:
+            //Створіть користувацький тип «Пристрій», який зберігатиме таку
+            //інформацію:
+            // назва пристрою;
+            // виробник пристрою;
+            // вартість.
+            //Для двох масивів пристроїв реалізуйте операції:
+            // різниця масивів;
+            // перетин масивів;
+            // об'єднання масивів.
+            //Критерій для проведення операцій — виробник пристрою.
+
+            Device[] devices1 = {new Device("device1","mfr1",100),
+                                 new Device("device2","mfr",200),
+                                 new Device("device3","mfr3",300),
+                                 new Device("device4","mfr4",400) };
+
+            Device[] devices2 = {new Device("device1","mfr1",100),
+                                 new Device("device2","mfr2",200),
+                                 new Device("device0","mfr0",10),
+                                 new Device("device4","mfr4",400) };
+            List<Device> result = new List<Device>();
+            Console.WriteLine("різниця масивів;");
+            result = devices1.Where(d1 => devices2.All(d2 => d2.Manufacturer != d1.Manufacturer)).ToList();
+            foreach (var device in result)
+            {
+                Console.WriteLine(device);
+            }
+
+            Console.WriteLine("перетин масивів;");
+            result = devices1.Where(d1 => devices2.Any(d2 => d2.Manufacturer == d1.Manufacturer)).ToList();
+            foreach (var device in result)
+            {
+                Console.WriteLine(device);
+            }
+
+            Console.WriteLine("об'єднання масивів;");
+            result = devices1.Union(devices2).ToList();
+            foreach (var device in result)
+            {
+                Console.WriteLine(device);
+            }
+
+        }
+    }
+}
